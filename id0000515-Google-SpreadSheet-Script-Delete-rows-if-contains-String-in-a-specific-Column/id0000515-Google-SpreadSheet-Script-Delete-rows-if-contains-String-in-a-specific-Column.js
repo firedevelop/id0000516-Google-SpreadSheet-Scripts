c9c15c7bd890dde86f59107c4a2d346ed4356bf8
firedevelop <firedevelop.com@gmail.com>
2019-08-14 14:48:19 +0200

    Add files via upload

--- id0000515-Google-SpreadSheet-Script-Delete-rows-if-contains-String-in-a-specific-Column/id0000515-Google-SpreadSheet-Script-Delete-rows-if-contains-String-in-a-specific-Column.js
+++ id0000515-Google-SpreadSheet-Script-Delete-rows-if-contains-String-in-a-specific-Column/id0000515-Google-SpreadSheet-Script-Delete-rows-if-contains-String-in-a-specific-Column.js
@@ -1,3 +1,7 @@
+// code: https://github.com/firedevelop/id0000516-Google-SpreadSheet-Scripts.git
+// video: https://youtu.be/EwxoVb9LgFY
+// Google SpreadSheet: https://docs.google.com/spreadsheets/d/1AflG3VgCFdqz1dRbP0JwGBrc5xXL9kwRBk13yEpekTM/edit#gid=1671517193
+
 var sheetName = "Products"; 
 var ss = SpreadsheetApp.getActive();
 var sheet = ss.getSheetByName(sheetName);
